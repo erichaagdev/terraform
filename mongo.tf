76e63212da694395506e2e4a54ca4652b99f6ed5
Gorlah <eah0592@gmail.com>
2021-08-21 20:56:43 -0500

    Add mongo

---
+++ mongo.tf
@@ -0,0 +1,96 @@
+variable "mongo-pod-selector" {
+  default = "mongo"
+}
+
+resource "kubernetes_namespace" "mongo-namespace" {
+  metadata {
+    name = "mongo"
+  }
+}
+
+resource "kubernetes_service" "mongo-service" {
+  metadata {
+    name      = "mongo-service"
+    namespace = kubernetes_namespace.mongo-namespace.metadata[0].name
+
+    labels = {
+      app = var.mongo-pod-selector
+    }
+  }
+
+  spec {
+    cluster_ip = "None"
+
+    port {
+      port = 27017
+    }
+
+    selector = {
+      app = var.mongo-pod-selector
+    }
+  }
+}
+
+resource "kubernetes_stateful_set" "mongo-stateful-set" {
+  metadata {
+    name      = "mongo-stateful-set"
+    namespace = kubernetes_namespace.mongo-namespace.metadata[0].name
+
+    labels = {
+      app = var.mongo-pod-selector
+    }
+  }
+
+  spec {
+    service_name = kubernetes_service.mongo-service.metadata[0].name
+    replicas     = 1
+
+    selector {
+      match_labels = {
+        app = var.mongo-pod-selector
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.mongo-pod-selector
+        }
+      }
+
+      spec {
+        termination_grace_period_seconds = 10
+
+        container {
+          name  = "mongo"
+          image = "mongo:5.0.1"
+
+          port {
+            container_port = 27017
+          }
+
+          volume_mount {
+            name       = "mongo-volume-claim"
+            mount_path = "/data/db"
+          }
+        }
+      }
+    }
+
+    volume_claim_template {
+      metadata {
+        name = "mongo-volume-claim"
+      }
+
+      spec {
+        access_modes = ["ReadWriteOnce"]
+
+        resources {
+          requests = {
+            storage = "2Gi"
+          }
+        }
+      }
+    }
+  }
+}
