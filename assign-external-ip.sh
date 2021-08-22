73e6b3b03a73b765bc850b401ce842e80032894e
Gorlah <eah0592@gmail.com>
2021-08-21 19:06:05 -0500

    Add zone to gcloud commands

--- assign-external-ip.sh
+++ assign-external-ip.sh
@@ -3,10 +3,10 @@
 gcloud container clusters get-credentials "$1" --zone="$2"
 
 vm_name=$(kubectl get nodes -o json | jq -r '.items[0].metadata.name')
-access_config=$(gcloud compute instances describe "$vm_name" --format json | jq -r '.networkInterfaces[].accessConfigs[].name' 2> /dev/null)
+access_config=$(gcloud compute instances describe "$vm_name" --zone="$2" --format json | jq -r '.networkInterfaces[].accessConfigs[].name' 2> /dev/null)
 
 if [ -n "$access_config" ]; then
-  gcloud compute instances delete-access-config "$vm_name" --access-config-name="$access_config"
+  gcloud compute instances delete-access-config "$vm_name" --zone="$2" --access-config-name="$access_config"
 fi
 
-gcloud compute instances add-access-config "$vm_name" --access-config-name="external-nat" --address="$3"
+gcloud compute instances add-access-config "$vm_name" --zone="$2" --access-config-name="external-nat" --address="$3"
