d68025e3afb154e321ba2bb9515f3cbc2092b923
Gorlah <eah0592@gmail.com>
2022-02-26 13:20:14 -0600

    Fix formatting

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   cluster_name = "coruscant"
-  domains = ["erichaag.dev"]
-  email = "eah0592@gmail.com"
+  domains      = ["erichaag.dev"]
+  email        = "eah0592@gmail.com"
 
   certificate_namespace = "certificates"
 }
